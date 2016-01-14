cc48ca18dacd725fea44f49060b86a1bc50d9b51
Chris Anderson <chrisanderson@Chriss-MacBook-Pro.local>
2016-01-13 21:08:49 -0600

    Answer 3

---
+++ 3.sql
@@ -0,0 +1,1 @@
+SELECT Customer.FirstName, Customer.LastName, invoice.invoiceid, invoice.invoicedate, invoice.billingcountry FROM Customer JOIN Invoice ON Customer.Customerid == Invoice.Customerid WHERE Customer.Country == "Brazil"
