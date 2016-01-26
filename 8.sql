d6cbd9e4696dd57d8109ba2dfe30571473838b5d
Chris Anderson <cl.anderson8@gmail.com>
2016-01-25 19:01:35 -0600

    Update 8.sql

--- 8.sql
+++ 8.sql
@@ -1,3 +1,5 @@
+--Provide a query that shows the Invoice Total, Customer name, Country and Sale Agent name for all invoices and customers.
+
 SELECT
 Employee.FirstName as 'Sales Agent', Employee.LastName as 'Sales Agent Last Name', tbl1.'Invoice Total' as 'invoice total', tbl1.'Customer First Name' as 'Customer First Name', tbl1.'Customer Last Name' as 'Customer Last Name', tbl1.'Country' as 'Country'
 FROM
