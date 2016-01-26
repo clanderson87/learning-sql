2ccc99dbd2fc70e4f4d5ae06a9f720e360bef6e9
Chris Anderson <cl.anderson8@gmail.com>
2016-01-25 18:47:52 -0600

    Update 8.sql

--- 8.sql
+++ 8.sql
@@ -1,11 +1,11 @@
-ELECT
-Employee.FirstName as 'Sales Agent', Employee.LastName as 'Sales Agent Last Name', lerft.'Invoice Total' as 'invoice total', lerft.'Customer First Name' as 'Customer First Name', lerft.'Customer Last Name' as 'Customer Last Name', lerft.'Country' as 'Country'
+SELECT
+Employee.FirstName as 'Sales Agent', Employee.LastName as 'Sales Agent Last Name', tbl1.'Invoice Total' as 'invoice total', tbl1.'Customer First Name' as 'Customer First Name', tbl1.'Customer Last Name' as 'Customer Last Name', tbl1.'Country' as 'Country'
 FROM
 (
 SELECT 
 Invoice.Total as 'Invoice Total', Customer.FirstName as 'Customer First Name', Customer.LastName as 'Customer Last Name', Customer.Country as 'Country', Customer.SupportRepId as 'Seller ID'
 FROM Invoice
 LEFT JOIN Customer ON Customer.CustomerId == Invoice.CustomerId
-) AS lerft
+) AS tbl1
 JOIN Employee
-ON lerft.'Seller ID'
+ON tbl1.'Seller ID'
